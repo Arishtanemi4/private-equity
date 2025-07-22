b30bf4e6f0d7baf30c56bbdc0168c12e246c420e
Arishtanemi4 <ksstubh.sonawane@gmail.com>
2025-07-22 15:13:37 +0530

    1. Preprocessing completed\n2. FY column added\nInsertion in Database to be started

--- poc/notebook/__init__.py
+++ poc/notebook/__init__.py
@@ -5,10 +5,16 @@ base_dir = os.getcwd()
 outer_dir = os.path.join(base_dir, "..")
 outer_dir = os.path.abspath(outer_dir)
 data_dir = os.path.join(outer_dir, 'data')
+raw_data_dir = os.path.join(data_dir, 'raw')
+processed_data_dir = os.path.join(data_dir, 'processed')
+pnl_dir = os.path.join(processed_data_dir, 'pnl')
+bal_dir = os.path.join(processed_data_dir, 'bal')
+cf_dir =os.path.join(processed_data_dir, 'cf')
 
 db_host = os.getenv('DB_HOST')
 db_port = os.getenv('DB_PORT')
-db_user_super = os.getenv('DB_USER')
-db_password_super = os.getenv('DB_PASSWORD')
+db_name = os.getenv('DB_NAME')
+db_user = os.getenv('DB_USER')
+db_password = os.getenv('DB_PASSWORD')
 
-db_connection_string_super = f"host {db_host} port {db_port} user {db_user_super} password {db_password_super}"
+dsn = f"host={db_host} port={db_port} dbname={db_name} user={db_user} password={db_password}"
