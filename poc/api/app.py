d9911cf7c73dfda50cfa02bbb793f318345854b9
Arishtanemi4 <ksstubh.sonawane@gmail.com>
2025-07-30 11:04:51 +0530

    Balance Sheet and Cash Flow Completed

--- poc/api/app.py
+++ poc/api/app.py
@@ -15,10 +15,28 @@ app.add_middleware(
 )
 
 
-@app.get("/macro/pnl")
+@app.get("/macro/profit-and-loss")
 def get_pnl(event: dict = Depends(macro.query_params)):
     try:
         response = macro.get_pnl(event)
+        return response
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@app.get("/macro/balance-sheet")
+def get_bs(event: dict = Depends(macro.query_params)):
+    try:
+        response = macro.get_bs(event)
+        return response
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@app.get("/macro/cash-flow")
+def get_cf(event: dict = Depends(macro.query_params)):
+    try:
+        response = macro.get_cf(event)
         return response
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
